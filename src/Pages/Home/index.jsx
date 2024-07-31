baa047ed5ac1f365b5a3f5d6a0658f732701c4e5
Martin <martin.cardozo500@gmail.com>
2024-07-31 15:29:05 -0400

    Integracion API fakeStore y implementacion a componente card

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,17 +1,29 @@
+import { useState, useEffect } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
 
-
 function Home() {
+   const [items, setItems] = useState(null)
 
+   useEffect(() => {
+      fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+   }, [])
 
-  return (
-  
+   return (
      <Layout>
         Home
-        <Card />
+        <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg m-5">
+        {
+         items?.map(item => (
+            <Card key={item.id} data={item} {...item} />
+         ))
+        }
+        </div>
+       
      </Layout>
-  )
+   )
 }
 
 export default Home
